aa42ebdc05ac4cd8c032cd077009e5f6eac8abc1
gautamswami <gautams5974@gmail.com>
2024-07-25 13:05:59 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Value = require('./models/Value');
+const Value = require('./models/value');
 
 const app = express();
 app.use(express.json());
